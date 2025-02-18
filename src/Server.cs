df00b4417c77714283cf2e9d103014ed7105ce7b
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 12:53:32 +0100

    add RESP parser

--- src/Server.cs
+++ src/Server.cs
@@ -33,12 +33,61 @@ while (true)
             Console.WriteLine($"Received: {request}");
 
             // Parse RESP formatted request
-            if (request.StartsWith("*1") && request.Contains("$4") && request.EndsWith("PING"))
+            var commandElements = ParseRESPArray(request);
+            if (commandElements != null && commandElements.Length > 0)
             {
-                byte[] response = Encoding.UTF8.GetBytes("+PONG\r\n");
-                await clientSocket.SendAsync(response, SocketFlags.None);
-                Console.WriteLine("Response sent: +PONG");
+                string command = commandElements[0].ToUpper();
+                if (command == "PING")
+                {
+                    byte[] response = Encoding.UTF8.GetBytes("+PONG\r\n");
+                    await clientSocket.SendAsync(response, SocketFlags.None);
+                    Console.WriteLine("Response sent: +PONG");
+                }
+                else if (command == "ECHO" && commandElements.Length == 2)
+                {
+                    string message = commandElements[1];
+                    byte[] response = Encoding.UTF8.GetBytes($"${message.Length}\r\n{message}\r\n");
+                    await clientSocket.SendAsync(response, SocketFlags.None);
+                    Console.WriteLine($"Response sent: {message}");
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown command: {command}");
+                }
             }
         }
     });
+}
+
+string[] ParseRESPArray(string request)
+{
+    if (request.StartsWith("*"))
+    {
+        int lengthEndIndex = request.IndexOf("\r\n");
+        if (lengthEndIndex > 1)
+        {
+            string lengthStr = request.Substring(1, lengthEndIndex - 1);
+            if (int.TryParse(lengthStr, out int length))
+            {
+                string[] elements = new string[length];
+                int currentIndex = lengthEndIndex + 2;
+                for (int i = 0; i < length; i++)
+                {
+                    if (request[currentIndex] == '$')
+                    {
+                        int bulkLengthEndIndex = request.IndexOf("\r\n", currentIndex);
+                        string bulkLengthStr = request.Substring(currentIndex + 1, bulkLengthEndIndex - currentIndex - 1);
+                        if (int.TryParse(bulkLengthStr, out int bulkLength))
+                        {
+                            currentIndex = bulkLengthEndIndex + 2;
+                            elements[i] = request.Substring(currentIndex, bulkLength);
+                            currentIndex += bulkLength + 2;
+                        }
+                    }
+                }
+                return elements;
+            }
+        }
+    }
+    return null;
 }
