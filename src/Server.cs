8e1d6be889d6da4d450b3a3a240789b33f1bc8fb
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 10:50:17 +0100

    [any message]

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 Console.WriteLine("Logs from your program will appear here!");
 
+await RunServerAsync();
 
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+async Task RunServerAsync()
+{
+	TcpListener server = new TcpListener(IPAddress.Any, 6379);
+
+	server.Start();
+	server.AcceptSocket(); // This line will block the program until a client connects
+
+	var clientSocket = server.AcceptSocket();
+	await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+}
