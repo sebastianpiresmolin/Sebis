e46e585edef82dc0a3985fa951d8774ffb634ac9
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 11:00:25 +0100

    add await

--- src/Server.cs
+++ src/Server.cs
@@ -2,11 +2,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+Console.WriteLine("Logs from your program will appear here!");
+
+
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 
 server.Start();
 server.AcceptSocket(); // This line will block the program until a client connects
 
 var clientSocket = server.AcceptSocket();
-byte[] response = Encoding.UTF8.GetBytes("+PONG\r\n");
-clientSocket.Send(response, SocketFlags.None);
+await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
