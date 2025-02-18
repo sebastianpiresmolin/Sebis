046650ed22cac725c9ba0c7d5bcde6f5c0b14c7b
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 11:23:55 +0100

    Refactored to be more concice

--- src/Server.cs
+++ src/Server.cs
@@ -12,12 +12,7 @@ server.Start();
 var clientSocket = server.AcceptSocket();
 while (clientSocket.Connected)
 {
-    byte[] buffer = new byte[1024];
-    int bytesRead = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
-    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-    Console.WriteLine(request);
-    if (request == "PING\r\n")
-    {
-        await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-    }
+    var buffer = new byte[1024];
+    await clientSocket.ReceiveAsync(buffer);
+    await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
 }
