c5ca014dc3d1fdb2629c0be0651f24eccbc8d673
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 11:04:38 +0100

    removed redundant server.AcceptSoccet

--- src/Server.cs
+++ src/Server.cs
@@ -8,7 +8,6 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 
 server.Start();
-server.AcceptSocket(); // This line will block the program until a client connects
 
 var clientSocket = server.AcceptSocket();
 await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
