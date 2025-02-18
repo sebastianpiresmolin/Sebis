775da08e1d635d9f092066317e3f2aa414ad52ce
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 10:52:01 +0100

    add PONG response to server

--- src/Server.cs
+++ src/Server.cs
@@ -1,19 +1,14 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 Console.WriteLine("Logs from your program will appear here!");
 
-await RunServerAsync();
 
-async Task RunServerAsync()
-{
-	TcpListener server = new TcpListener(IPAddress.Any, 6379);
+TcpListener server = new TcpListener(IPAddress.Any, 6379);
 
-	server.Start();
-	server.AcceptSocket(); // This line will block the program until a client connects
+server.Start();
+server.AcceptSocket(); // This line will block the program until a client connects
 
-	var clientSocket = server.AcceptSocket();
-	await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-}
+var clientSocket = server.AcceptSocket();
+clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
