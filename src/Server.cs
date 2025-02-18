8760e48e62271162923297b586be1dbdff073f7e
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-18 11:21:56 +0100

    Add while loop that runs as long as the client is connected

--- src/Server.cs
+++ src/Server.cs
@@ -10,4 +10,14 @@ TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
 var clientSocket = server.AcceptSocket();
-await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+while (clientSocket.Connected)
+{
+    byte[] buffer = new byte[1024];
+    int bytesRead = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine(request);
+    if (request == "PING\r\n")
+    {
+        await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    }
+}
